778cdee5a7aea56aaaa231d22278b53f90fbee90
joo <joo@jooui-MacBookPro.local>
2025-11-24 16:04:46 +0900

    create userInfo.java

---
+++ src/main/java/org/example/toy_login/userInfo.java
@@ -0,0 +1,22 @@
+package org.example.toy_login;
+
+public class userInfo{
+    private String name;
+    private String password;
+
+    public userInfo(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
